3d172bfd034c98db3e5b8dbf5fb507b676910b0b
coluccifrancesco <coluccif2002@gmail.com>
2025-07-26 01:43:17 +0200

    Update comments in App component to outline product detail routing and loading state management

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -39,3 +39,16 @@ export default App
 // Bonus
 // Centralizziamo la Navbar usando un componente Layout
 // Gestiamo la classe active per i link attivi nella Navbar
+
+
+
+// Consegna
+// Completiamo il nostro routing aggiungendo la pagina di dettaglio prodotto!
+// Obiettivi
+// Nella pagina Prodotti, ogni prodotto deve essere cliccabile (usa <Link>)
+// Aggiungiamo la pagina di dettaglio per ogni prodotto, con le informazioni prese da https://fakestoreapi.com/products/:id
+// Configuriamo il routing dinamico per leggere l’id del prodotto dalla URL
+// Bonus
+// Aggiungiamo nella pagina di dettaglio dei pulsanti per navigare al prodotto precedente o successivo (usando useNavigate() programmaticamente)
+// Gestire la pagina 404 - create un componente 404 e usatelo nella rotta catch all
+// getire lo stato di caricamento della pagina mentre aspettiamo la risposta alla chiamata ajax
