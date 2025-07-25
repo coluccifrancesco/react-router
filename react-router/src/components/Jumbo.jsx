a4b66f3b25dbb6248211692c57e19c9aef218879
coluccifrancesco <coluccif2002@gmail.com>
2025-07-25 02:37:58 +0200

    Enhance About Us page with team member details and integrate Jumbo component

--- react-router/src/components/Jumbo.jsx
+++ react-router/src/components/Jumbo.jsx
@@ -8,9 +8,14 @@ export default function Jumbo() {
                     <div className="h-100 p-5 text-black bg-transparent">
                         <h2>Stile Toscano - Moda che Parla di Te</h2>
                         <p>Scopri il tuo look unico nel cuore di Grosseto! Abbigliamento di tendenza, qualità artigianale e stile senza tempo per uomo e donna. Vieni a vivere la moda che racconta chi sei!</p>
-                        <NavLink to='/products' className="nav-link"><button className="btn btn-outline-dark" type="button">
-                            Scopri gli articoli
-                        </button></NavLink>
+                        <div className='d-flex align-items-center justify-content-start'>
+                            <NavLink to='/products' className="nav-link"><button className="btn btn-primary" type="button">
+                                Scopri gli articoli
+                            </button></NavLink>
+                            <NavLink to='/about_us' className="nav-link"><button className="btn btn-warning ms-5 " type="button">
+                                La nostra storia
+                            </button></NavLink>
+                        </div>
                     </div>
                 </div>
             </div>
--- react-router/src/pages/AboutUs.jsx
+++ react-router/src/pages/AboutUs.jsx
@@ -1,11 +1,71 @@
 import Jumbo from '../components/Jumbo'
 
 export default function AboutUs(){
-    return(
-        <>
 
+    const team = [  
+   {
+    name: "Alice",
+    age: 28,
+    profession: "Web Developer",
+    hobbies: "coding, reading, traveling",
+    id: 1
+  },
+  {
+    name: "Marco",
+    age: 35,
+    profession: "Graphic Designer",
+    hobbies: "drawing ,photography, cycling",
+    id: 2
+  },
+  {
+    name: "Sophia",
+    age: 24,
+    profession: "Data Analyst",
+    hobbies: "puzzle games, hiking, cooking",
+    id: 3
+    
+  },
+  {
+    name: "Luca",
+    age: 30,
+    profession: "Marketing Specialist",
+    hobbies: "social media, writing, football",
+    id: 4
+  }
+    ];
+
+    return(
+        <main className='mt-5 pt-5'>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+                <h1>Scopri il nostro Team</h1>
+                
+                <div className='container'>
+                    <ul className="row list-unstyled">
+                        
+                        {team.map((person)=>{
+                            return <li key={person.id} className="col col-12 col-md-6 col-lg-3 my-4">
+                           
+                                <div className="card">
+                                    <img src='https://picsum.photos/200'/>
+                                    <div className='card-body'>
+                                        <h1 className='card-title'>{person.name}</h1>
+                                            
+                                        <div className='d-flex justify-content-start align-items-center gap-4 mt-3 mb-0'>
+                                            <p className='mb-2'>{person.age} anni</p>
+                                            <p className='mb-2'>Ruolo: {person.profession}</p>
+                                        </div>
+                                            
+                                        <p>Hobby: {person.hobbies}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        })}
+                    
+                    </ul>
+                </div>
+            </div>
 
             <Jumbo />
-        </>
+        </main>
     )
 }
