4b16113982bf71024ebf9d8c564a6df9647c72bb
coluccifrancesco <coluccif2002@gmail.com>
2025-07-28 09:47:53 +0200

    Refactor Footer component layout for improved responsiveness and alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ export default function Header(){
     return(
         <footer>
             <div className="container my-5">
-                <div className="row align-items-center">
+                <div className="row align-items-center justify-content-between">
                         
-                    <div className="col-4 d-flex align-items-center flex-column p-3">
+                    <div className="col col-12 col-sm-4 d-flex d-sm-block align-items-center flex-column p-3 mb-4">
                             
                         <h5 className="mb-3 text-primary">Privacy e Legali</h5>
                             
@@ -30,14 +30,11 @@ export default function Header(){
                             
                     </div>
 
-                    <div className="col-4 d-flex align-items-center flex-column p-3">
+                    <div className="col col-12 col-sm-4 d-flex d-sm-block align-items-center flex-column p-3 mb-4">
                             
                         <h5 className="mb-3 text-primary">Guide e Articoli</h5>
                             
                         <ul className="list-group list-group-flush d-flex justify-content-center align-items-start flex-column">
-                            <li className="list-group-item bg-transparent border-0 text-light px-0">
-                                <NavLink to='/products' className=" text-decoration-none">Come Acquistare</NavLink>
-                            </li>
                             <li className="list-group-item bg-transparent border-0  px-0">
                                 <NavLink to='/products' className=" text-decoration-none">Metodi di Pagamento</NavLink>
                             </li>
@@ -54,7 +51,7 @@ export default function Header(){
                         
                     </div>
 
-                    <div className="col-4 d-flex align-items-center flex-column p-3">
+                    <div className="col col-12 col-sm-4 d-flex d-sm-block align-items-center flex-column p-3 mb-4">
                         
                         <h5 className="mb-3 text-primary">Chi Siamo</h5>
                             
